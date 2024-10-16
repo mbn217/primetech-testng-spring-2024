e9c8d03e791e7a6426dd0b6e28d1db94e41554fb
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-15 21:04:13 -0400

    adding some code

--- src/test/java/testcases/Calculator.java
+++ src/test/java/testcases/Calculator.java
@@ -19,20 +19,6 @@ public class Calculator {
         System.out.println("Hello");
     }
 
-    @Test
-    public void loginWithValidCredentials(){
-        /**
-         * 1.	Navigate to the login page.
-         * 2.	Enter a valid email address “entityadmin@primetechschool.com”.
-         * 3.	Enter a valid password “primetech@school”.
-         * 4.	Click on the Login button.
-         * 5.	Verify the user is redirected to the dashboard.
-         * 6.	Verify a success message is displayed.
-         */
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("sauce demo url");
-        WebElement username = driver.findElement(By.xpath("xpath"));
-    }
 
 }
