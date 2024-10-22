2692a62f8911fe7231ca5fcc8fceb2835a65e354
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-22 19:05:02 -0400

    xml

--- src/test/java/abc/Calculator.java
+++ src/test/java/abc/Calculator.java
@@ -1,10 +1,6 @@
-package testcases;
+package abc;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Calculator {
