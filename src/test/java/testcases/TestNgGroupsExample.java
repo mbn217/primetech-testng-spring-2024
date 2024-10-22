c21ba2cb3c458703cd186202e7add94e0fead3b6
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-21 21:03:54 -0400

    soft asser vs hard assert

---
+++ src/test/java/testcases/TestNgGroupsExample.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TestNgGroupsExample {
+
+
+    @Test
+    public void verifyLogin(){
+        System.out.println("Verify Login");
+    }
+
+    @Test
+    public void verifySignOut(){
+        System.out.println("Verify Signout");
+    }
+
+    @Test
+    public void verifyDashboard(){
+        System.out.println("verify dashboard");
+    }
+
+
+}
