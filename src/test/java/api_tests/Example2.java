95dd073cb5efc72e2507a2eb693a6f89e2ef55bc
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-29 19:42:31 -0400

    restassured

--- src/test/java/api_tests/Example2.java
+++ src/test/java/api_tests/Example2.java
@@ -1,4 +1,26 @@
 package api_tests;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
 public class Example2 {
+
+
+
+    @Test
+    public void testCaseOne(){
+        baseURI = "https://reqres.in";
+        given()
+                .get("/api/users?page=2")
+                .then()
+                .statusCode(200);
+
+
+
+
+    }
+
+
 }
