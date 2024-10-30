225caaef4231cb796b62f7e2c331c2e38b8002c7
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-29 21:03:30 -0400

    restassured

--- src/test/java/api_tests/Example2.java
+++ src/test/java/api_tests/Example2.java
@@ -1,10 +1,13 @@
 package api_tests;
 
+
 import org.testng.annotations.Test;
 
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 
 public class Example2 {
 
@@ -24,6 +27,13 @@ public class Example2 {
     @Test
     public void testCaseTwo(){
         baseURI = "https://reqres.in";
+        given()
+                .get("/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("data[1].id", equalTo(8) )
+                .body("support.url", equalTo("https://reqres.in/#support-heading"))
+                .body("data.first_name" , hasItems("Michael","Lindsay","Byron"));
 
 
     }
