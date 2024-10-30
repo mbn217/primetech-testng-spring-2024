e7558cd8b89d96eef73474e4a744479201ce90b1
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-29 21:34:26 -0400

    restassured

--- src/test/java/api_tests/Example2.java
+++ src/test/java/api_tests/Example2.java
@@ -1,11 +1,14 @@
 package api_tests;
 
 
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 
@@ -31,9 +34,31 @@ public class Example2 {
                 .get("/api/users?page=2")
                 .then()
                 .statusCode(200)
-                .body("data[1].id", equalTo(8) )
+                .body("data[1].id", equalTo(8))
                 .body("support.url", equalTo("https://reqres.in/#support-heading"))
                 .body("data.first_name" , hasItems("Michael","Lindsay","Byron"));
+
+
+    }
+
+    @Test
+    public void testCaseThree(){
+        Response response = get("https://reqres.in/api/users?page=2");
+        int totalPages = response.path("total_pages");
+        System.out.println(totalPages);
+        String supportURL = response.path("support.url");
+        System.out.println(supportURL);
+
+        String firstElementEmail = response.path("data[0].email");
+        System.out.println(firstElementEmail);
+        System.out.println("------------------");
+
+        //We use jsonPath() method to call getList() that will return a list of element
+         List<String> listOfEmails = response.jsonPath().getList("data.email");
+
+        System.out.println(listOfEmails);
+
+        Assert.assertEquals(firstElementEmail , "michael.lawson@reqres.in");
 
 
     }
